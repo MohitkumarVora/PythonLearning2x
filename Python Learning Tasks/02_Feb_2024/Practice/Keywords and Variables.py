dc9ffdf21db03daa756125002f736aa9c283f871
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-15 15:23:34 +0530

    Add some other approach to use Variables

--- Python Learning Tasks/02_Feb_2024/Practice/Keywords and Variables.py
+++ Python Learning Tasks/02_Feb_2024/Practice/Keywords and Variables.py
@@ -2,6 +2,12 @@
 # Keyword - reserved words.
 # Variable name = Variable value
 
+#define variable name in different ways
+PI = 3.14 # Constant = Uppercase
+temp_pi = 8.98 # variable
+print() # function name is always smaller case
+
+
 age = 45
 print(age)
 age = "Mohit"
@@ -32,3 +38,16 @@ print(As)
 #print list of Keywords which was use by Python
 import keyword
 print(keyword.kwlist)
+
+# Take a User Input and Print the details
+name = input("Enter your Name")
+print("This is your name", name)
+
+#Variable Override
+a = 10
+b = a
+c = b
+a = "Mohit"
+print(a)
+print(b)
+print(c)
