6e0600c68fc42d5e4322bf93d320bca46fca9472
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-15 15:25:54 +0530

    practice Data types and write some examples

---
+++ Python Learning Tasks/02_Feb_2024/Practice/Data Type Learning.py
@@ -0,0 +1,39 @@
+
+
+#NOTE: any Variable is default set to str(String) excluding List, tuple, set and dictionary
+
+# Numbers
+# Integer (whole)
+age = 78
+age = -90
+roll_number = 123
+phone_number = 9876543210
+print(type(age))
+
+# Float - decimal numbers
+pi = 3.14
+gst = 0.18
+pi = -3.14
+
+print(type(pi))
+
+# Boolean
+isMarried = True
+isSantoshMarried = False
+print(type(isMarried))
+
+# Complex Data Type
+# i - iota = sq -1 , j
+a = 1+7j
+print(type(a))
+
+#Python is used AI, ML, DS, Math - they need
+
+# str Data Type(String)
+name = "Mohit"
+name2 = 'Vora'
+char = 'L'
+print(type(name))
+print(type(name2))
+print(type(char))
+
