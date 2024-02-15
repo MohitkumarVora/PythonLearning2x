7314a57f05a9f718f044e709d13836a8b0539e56
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-15 15:04:47 +0530

    practice Keywords and Variable

---
+++ Python Learning Tasks/02_Feb_2024/Practice/Keywords and Variables.py
@@ -0,0 +1,34 @@
+# Keyword and Identifier
+# Keyword - reserved words.
+# Variable name = Variable value
+
+age = 45
+print(age)
+age = "Mohit"
+print(age)
+age = 'C'
+print(age)
+
+# class = "Mohit"
+_123 = 789
+print(_123)
+
+_ = "Akash"
+print(_)
+
+#______________________________________
+#Python is Case sensitive Language so Upper and Lower case Variables are consider as different
+age = 45
+Age = 56
+
+#class = "Mohit"
+Class = "mohit"
+print(Class)
+
+#as = "Mohit"
+As = "At"
+print(As)
+
+#print list of Keywords which was use by Python
+import keyword
+print(keyword.kwlist)
