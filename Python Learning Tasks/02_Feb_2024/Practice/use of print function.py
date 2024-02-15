96aeaf164a85b577b4c3574740f262327769c51e
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-15 15:23:58 +0530

    Add some other approach to use print function

--- Python Learning Tasks/02_Feb_2024/Practice/use of print function.py
+++ Python Learning Tasks/02_Feb_2024/Practice/use of print function.py
@@ -1,5 +1,8 @@
-print("Pramod","Dutta","Amit","Sharma")
-result = max(3,4)
+# print(self, *args, sep=' ', end='\n', file=None)
+
+print("Mohit", "Vora", "Akash", "Khatri")
+result = max(3, 4)
 print(result)
 
-# print(self, *args, sep=' ', end='\n', file=None)
+a, b, c = 10, True, "Pramod"
+print(a, b, c)
