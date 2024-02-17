f1f714add949b4eaf55bed9c2483719df519838f
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-17 17:13:57 +0530

    create program to set float type when div any high number

---
+++ Python Learning Tasks/05_Feb_2024/Practice/print float value when div any high number.py
@@ -0,0 +1,6 @@
+f1 = 10  # Integer type
+f2 = 20  # Integer type
+f3 = f2/f1  # set Float type when div any high number
+# Python Int - Int -> div anything high number chance float number
+print(type(f3))
+print(f3)
