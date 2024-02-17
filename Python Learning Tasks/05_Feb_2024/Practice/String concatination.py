d7ceacd36e50f81ef5d6c0dfd004404f8dad70f6
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-17 15:54:48 +0530

    String concatination program when variable data type not assign

---
+++ Python Learning Tasks/05_Feb_2024/Practice/String concatination.py
@@ -0,0 +1,18 @@
+# Take the 2 input from the user and add them in third variable
+
+# step1 = Take input
+# step2 = Sum
+
+num1 = input("Enter the first Number: ")
+num2 = input("Enter the second Number: ")
+
+print(type(num1))
+print(num1)
+print(type(num2))
+print(num2)
+num3 = num1 + num2
+
+# Default Variable will always consider as the "str" data type
+# str + str -> concat -> combine them or join them
+print(type(num3))
+print(num3)
