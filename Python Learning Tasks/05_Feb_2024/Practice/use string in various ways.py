f0b47211db6955bf6ddc050213b03693dff6fe93
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-17 17:26:45 +0530

    use string type in various ways

---
+++ Python Learning Tasks/05_Feb_2024/Practice/use string in various ways.py
@@ -0,0 +1,21 @@
+# Strings is basically a bunch of Characters
+# define string by using (double cot)"" or (single cot)''
+name1 = "Mohit"
+name2 = 'Mohit'
+print(name1)
+print(name2)
+print(type(name1))
+print(type(name2))
+
+# raw String
+# This will be helpful in the dir paths
+Dir = r'C:\Program Files\PythonProject'
+print(Dir)
+
+
+# format String
+first_name = "Akash"
+last_name = "Khatri"
+age = 40
+isMarried = True
+print(f'My friend name is {first_name} {last_name}, his age is {age} and he is {isMarried} ')
