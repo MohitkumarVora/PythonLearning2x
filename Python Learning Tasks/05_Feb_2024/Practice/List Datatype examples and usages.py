ba8baa4b022ff30f09c509cec715e48a8f8e5610
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-19 16:50:56 +0530

    Add list Data type examples

---
+++ Python Learning Tasks/05_Feb_2024/Practice/List Datatype examples and usages.py
@@ -0,0 +1,18 @@
+# Int, Float, String
+
+# Sequence - List
+#  List data type is define by Square bracket like "[]"
+# Collection of Items
+# It can be of different data types
+
+# List with only String data type
+fruit_names = ["Oranges", "Apple", "Kiwi", "Watermelon"]
+print("print list length: ", len(fruit_names))
+print("print list 0 index item: ", len(fruit_names[0]))
+print("print list 2 index item: ", fruit_names[2])
+print("print list last index item: ", fruit_names[-1])
+
+# list with different data types like (Integer, Boolean, Float, String)
+list1 = [1, 2, 3, 4, True, 3.14, "Mohit"]
+print("print list1 variable data type: ", type(list1))  # <class 'list'>
+print("print list1 variable length: ", len(list1))  # <class 'list'>
