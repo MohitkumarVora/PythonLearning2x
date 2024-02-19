47fb853dec87656bf1d49404bd3ba8187236e779
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-19 17:07:27 +0530

    Escape Sequence examples

---
+++ Python Learning Tasks/05_Feb_2024/Practice/Escape Sequence.py
@@ -0,0 +1,25 @@
+# Escape Sequence Examples:
+
+# \n -> new ine
+print("a\nb")
+print("a\tb")
+# \a - bell
+# \t - tab char
+# \f - formfeed
+
+
+# Row Escape using 'r'
+dir_path = r'C:\Program Files\PythonProject'
+print("print row escape string: ", dir_path)
+
+# Bell Escape using '\a'
+dir_path1 = 'C:\anter\apvc.md'
+print("print bell escape string: ", dir_path1)
+
+# New line using '\n'
+dir_path2 = 'C:\nnter\npvc.md'
+print("print New line escape string: ", dir_path2)
+
+# tab using 't'
+dir_path3 = 'C:\tnter\tpvc.md'
+print("print Tab escape string: ", dir_path3)
