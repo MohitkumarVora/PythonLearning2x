f5aa57a0bec9767ad6fa0ba191b527922547c2b5
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-20 11:32:23 +0530

    Add literals examples

---
+++ Python Learning Tasks/07_Feb_2024/Practice/Literals examples.py
@@ -0,0 +1,16 @@
+# Literals
+
+# var_name = var_value
+# var_name -> Identifier
+# var_val -> Literals
+
+# Literals are the actual values assigned
+# Literals can be Numeric and Non-Numeric.
+name = "Mohit"
+age = 35
+pi = 3.14
+char = 'M'
+is_married = True
+have_car = None
+my_list = []
+my_set = {}
