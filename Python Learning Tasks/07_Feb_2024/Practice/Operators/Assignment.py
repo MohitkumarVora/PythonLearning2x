7332924577b3aa87c395378804d6035ae5bb57af
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-20 14:28:22 +0530

    update example by using datetime import and use function chaining

--- Python Learning Tasks/07_Feb_2024/Practice/Operators/Assignment.py
+++ Python Learning Tasks/07_Feb_2024/Practice/Operators/Assignment.py
@@ -1,6 +1,8 @@
+from datetime import date
+
 
 user_name = "Mohit"
 user_age = 35
-user_DOB = "03/30/1988"
+user_DOB = date(1988, 3, 30).strftime('%Y/%m/%d')
 
-print("User details is: ", user_name, user_age, user_DOB)
+print("User details is:\n", "Name: ", user_name, "Age: ", user_age, "DOB: ", user_DOB)
