e3ce65c8b5d2de702b3bd5a5c874a72c484b95f3
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-21 00:44:32 +0530

    Calculate the square and cube of given number

---
+++ Python Learning Tasks/07_Feb_2024/Tasks/Calculate Square and Cube.py
@@ -0,0 +1,8 @@
+# Write a Python script that calculates the square and cube of a given number.
+
+number = int(input("Enter the Number: "))
+
+print("Calculate the square is: ", number * number)
+print("Calculate the Square is: ", number ** 2)
+print("Calculate the Cube is: ", number * number * number)
+print("Calculate the Cube is: ", number ** 3)
