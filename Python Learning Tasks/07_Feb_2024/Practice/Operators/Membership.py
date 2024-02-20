7e77477a67921e370233b080ebd93bdda8989c77
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-20 23:38:21 +0530

    Membership Operators Examples

---
+++ Python Learning Tasks/07_Feb_2024/Practice/Operators/Membership.py
@@ -0,0 +1,8 @@
+x = ["apple", "banana"]
+
+
+# "in" membership Operator:
+print("banana" in x)  # returns True because a sequence with the value "banana" is in the list
+
+# "not in" membership Operator:
+print("pineapple" not in x)  # returns True because a sequence with the value "pineapple" is not in the list
