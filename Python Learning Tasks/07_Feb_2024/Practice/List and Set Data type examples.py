506559090aed946397105989a90b61c274d4fce7
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-20 11:27:01 +0530

    write list and set data type example

---
+++ Python Learning Tasks/07_Feb_2024/Practice/List and Set Data type examples.py
@@ -0,0 +1,12 @@
+
+
+# Lists - Unordered collection of Items
+my_daily_Task = ["wakeup", "exercise", "office", "client call", "python revision and practice"]
+print("I am more excited of doing: ", my_daily_Task[4], "daily routine.")
+print("print my_daily_task data type: ", type(my_daily_Task))
+
+# sets - Unordered Collection of Unique items
+my_favorite_fruits = {"apple", "kiwi", "pineapple", "orange", "banana", "apple"}
+print("My favorite fruit is: ", my_favorite_fruits)
+print("print my_favorite_fruits data type: ", my_favorite_fruits)
+
