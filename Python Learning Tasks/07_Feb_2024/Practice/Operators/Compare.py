9c2c7fc59f4cd4dafba0a8fb170be95d3cee13bb
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-20 14:15:19 +0530

    Comparison operator examples

---
+++ Python Learning Tasks/07_Feb_2024/Practice/Operators/Compare.py
@@ -0,0 +1,26 @@
+num1 = 5
+num2 = 5
+num3 = 8
+
+if num1 == num2:  # equal to operator
+    print("both are equal numbers")
+
+
+if num1 != num3:  # not equal to operator
+    print("both are not equal numbers")
+
+
+if num3 > num1:  # greater than operator
+    print("num3 is greate than num1")
+
+
+if num2 < num3:  # less than operator
+    print("num2 is less than num3")
+
+
+if num3 >= num1:  # greater than equal to operator
+    print("num3 is greater than equal to num1")
+
+
+if num2 <= num3:  # less than equal to operator
+    print("num2 is less than equal to num3")
