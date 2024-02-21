bc99ed4cde6f27ec9630b9d81b3bfd9f6e611307
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-22 01:03:09 +0530

    learn break keyword use and also add 12_feb_2024 summary to cover.

---
+++ Python Learning Tasks/12_Feb_2024/Practice/Summary.py
@@ -0,0 +1,20 @@
+# Completed Topics:
+"""
+For
+# While
+# If else
+# If elif else
+# Switch - Python x -> MATCH and case
+"""
+
+# Need to Complete Today:
+"""
+# Break, Continue
+# Functions
+# 1. Non Return Type
+# 2. Return Type
+# 3. Built Functions
+# *args and **kwargs
+# Lambda Expresson - solve problems of writing long functions -> oneline
+# Last - Question -> Anwsers
+"""
