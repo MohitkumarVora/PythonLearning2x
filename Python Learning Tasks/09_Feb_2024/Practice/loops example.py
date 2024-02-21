f3d9a89dd28abc8626f9be3c853f74d53b51b507
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-21 11:17:31 +0530

    for and while loops examples

---
+++ Python Learning Tasks/09_Feb_2024/Practice/loops example.py
@@ -0,0 +1,33 @@
+# Loop ?
+# # Repeat a block of code multiple times
+
+for i in range(5):  # 0-4
+    print(i)
+
+print(" ----- ")
+
+for i in range(1, 5):  # 1-4
+    print(i)
+
+print(" ----- ")
+
+for i in range(3, 5):  # 3-4
+    print(i)
+
+print(" ----- ")
+
+for i in range(1, 10, 2):  # 1-9 -> 1,3,5,7,9
+    print(i)
+
+# range(start, stop, step)
+
+# Print the value of Hello Pramod 10 times
+for i in range(1, 11):  # 1-10
+    print("Hello Pramod", i)
+
+
+# while
+i = 0
+while i < 5:
+    print(i)
+    i = i + 1  # i++ in python - we don't have
