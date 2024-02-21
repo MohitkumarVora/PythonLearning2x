529a685a1f7cce0106c9cd1963de9aa3def44e89
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-22 00:45:21 +0530

    add Fibonacci sequence using function

--- Python Learning Tasks/09_Feb_2024/Tasks/Fibonacci Sequence using only for lopp and if condition .py
+++ Python Learning Tasks/09_Feb_2024/Tasks/Fibonacci Sequence using only for lopp and if condition .py
@@ -1,3 +1,4 @@
+# Using only for loop and if condition
 limit = int(input("Enter the limit for Fibonacci sequence: "))
 fib_sequence = [0, 1]  # Initialize Fibonacci sequence with first two numbers
 
@@ -14,3 +15,17 @@ else:
         fib_sequence = fib_sequence + [next_fib]  # Using list concatenation instead of append
 
 print("Fibonacci sequence up to", limit, ":", fib_sequence)
+
+# Using Function
+
+
+def fibonacci_series(number):
+    fib_seq = [0, 1]
+    for seq in range(2, number):
+        new_number = fib_seq[-1] + fib_seq[-2]
+        fib_seq.append(new_number)
+    print("Fibonacci series is:", fib_seq)
+
+
+Number = int(input("Enter the level of fibonacci series: "))
+fibonacci_series(Number)
