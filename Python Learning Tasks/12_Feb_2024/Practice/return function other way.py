2989bbc8548b858117c8b45661babd4cd0a8b73e
Mohitkumar Vora <mohitv88@gmail.com>
2024-02-22 16:12:56 +0530

    return function other way

---
+++ Python Learning Tasks/12_Feb_2024/Practice/return function other way.py
@@ -0,0 +1,19 @@
+def sum_of_num(a, b):
+    return a + b
+
+
+result = sum_of_num(3, 4)
+print(result)
+
+result = sum_of_num(22.4, 49.89)
+print(result)
+
+result = sum_of_num("Mohit ", "Vora")
+print(result)
+
+# # TypeError: can only concatenate str (not "int") to str
+# result = sum_of_num("Mohit", 123)
+# print(result)
+
+a = None
+print(a)
