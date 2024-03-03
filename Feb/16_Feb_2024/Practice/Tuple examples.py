166e809a9a3ae873c74d96a74063e568b78f004a
Mohitkumar Vora <mohitv88@gmail.com>
2024-03-03 18:48:36 +0530

    multiple tuple examples in single file

--- Feb/16_Feb_2024/Practice/Tuple examples.py
+++ Feb/16_Feb_2024/Practice/Tuple examples.py
@@ -89,4 +89,3 @@ print(name)
 # String is a bunch of character, immutable
 # tuple is list of any data type items which can't changed.
 # list is collection of items, which can be changed, you can duplicate
-
