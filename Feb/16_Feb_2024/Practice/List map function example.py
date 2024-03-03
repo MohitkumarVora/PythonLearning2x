03c45538179baf9f2123aa4fa480284bd6d0911b
Mohitkumar Vora <mohitv88@gmail.com>
2024-03-03 18:34:26 +0530

    list map function example using import match

--- Feb/16_Feb_2024/Practice/List map function example.py
+++ Feb/16_Feb_2024/Practice/List map function example.py
@@ -1,4 +1,7 @@
+import math
+
 # Map()
+
 
 def sq_of_number(num):
     return num ** 2
@@ -14,6 +17,20 @@ numbers = [1, 2, 3, 4, 5]
 # 2. execute the function on it.
 # 3. Return same number of elements ( list)
 
-sq_numbers = list(map(sq_of_number,numbers))
+sq_numbers = list(map(sq_of_number, numbers))
 # 1,4,9, 16,25
 print(sq_numbers)
+
+print("-------------------------------------")
+
+
+def sq_rt(num):
+    return math.sqrt(num)
+    # return math.cbrt(num)
+
+
+# list, set, tuple, dic
+
+my_list = [21, 34, 54]
+sq_list = list(map(sq_rt, my_list))
+print(sq_list)
