c3ea9edaf8d3be7140b54b99e942164d2ae12537
Mohitkumar Vora <mohitv88@gmail.com>
2024-03-08 11:28:23 +0530

    student class exaple

---
+++ Feb/19_Feb_2024/Practice/Student Class example.py
@@ -0,0 +1,28 @@
+# Automation Tester Blueprint (Py System
+# Class -Students,Courses, Payments - Razorpay, Stipe, Instamojo
+
+# Object
+# Student (A/B) - > Ajay, Faraz, Mohit
+# Course (A/B) -> Java, Python, API, Jmeter
+# Payments (A/B) - Razorpay, Stipe, Instamojo
+
+
+class Student:
+    name = None
+    phone_no = None
+
+    def watch_recordings(self):
+        print("Reco")
+
+    def do_assignment(self):
+        print("Reco")
+
+    def do_coding_q(self):
+        print("Reco")
+
+
+Faraz = Student()
+
+Ajay = Student()
+
+Mohit = Student()
